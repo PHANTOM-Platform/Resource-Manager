7cc850c770a8695b5dfea7d1b409f4b5d77a4026
Jose Miguel Montanana <jmmontanana@gmail.com>
2019-05-16 16:56:25 +0200

    Add files via upload

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "PHANTOM Resource Manager API",
+  "version": "1.0.0",
+  "description": "...",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-05-16T14:45:28.298Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
